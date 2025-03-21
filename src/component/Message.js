66b21ded37e330161250933be5f4c52a1556bf2c
Aleks <korparativ@mail.ru>
2025-03-21 20:31:42 +1200

    kommit 2

--- src/component/Message.js
+++ src/component/Message.js
@@ -1,8 +1,10 @@
+import CurrentTime from './CurrentTime';
 import './Message.css';
 function Message(props) {
     return ( 
         <div className="Message">
             <h2>{props.text}</h2>
+            <CurrentTime />
         </div>
      );
 }
